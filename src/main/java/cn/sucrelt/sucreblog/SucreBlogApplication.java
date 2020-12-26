63f567159347d0972eb1470874367d02709352ee
tangtangsama <1462339680@qq.com>
2020-12-26 14:27:48 +0800

    生成项目的基本文件

---
+++ src/main/java/cn/sucrelt/sucreblog/SucreBlogApplication.java
@@ -0,0 +1,13 @@
+package cn.sucrelt.sucreblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucreBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SucreBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/sucrelt/sucreblog/SucreBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sucrelt.sucreblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucreBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
