319404a83212bf48d450cb5b1d3fa5b9b1506796
tangtangsama <1462339680@qq.com>
2020-12-28 17:09:24 +0800

    启动类加上MyBatis扫描注解

--- src/main/java/cn/sucrelt/sucreblog/SucreBlogApplication.java
+++ src/main/java/cn/sucrelt/sucreblog/SucreBlogApplication.java
@@ -1,8 +1,10 @@
 package cn.sucrelt.sucreblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.sucrelt.sucreblog.dao")
 @SpringBootApplication
 public class SucreBlogApplication {
 
