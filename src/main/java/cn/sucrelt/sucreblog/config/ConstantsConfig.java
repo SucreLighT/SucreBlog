b12e045af514bc398ca25bb61176f70e16c9b4d4
tangtangsama <1462339680@qq.com>
2020-12-29 09:50:50 +0800

    添加常量配置类

---
+++ src/main/java/cn/sucrelt/sucreblog/config/ConstantsConfig.java
@@ -0,0 +1,13 @@
+package cn.sucrelt.sucreblog.config;
+
+/**
+ * @description: 常量配置类
+ * @author: sucre
+ * @date: 2020/12/29
+ * @time: 09:24
+ */
+
+public class ConstantsConfig {
+    //上传文件的默认url前缀，根据部署设置自行修改
+    public final static String FILE_UPLOAD_DIC = "/opt/deploy/upload/";
+}
