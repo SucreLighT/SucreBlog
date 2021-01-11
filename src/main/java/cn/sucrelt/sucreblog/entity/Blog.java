a8d38a4bfa83c48ec975f668fa2d1fc1fc2aa144
tangtangsama <1462339680@qq.com>
2021-01-11 18:49:19 +0800

    添加博客实体类

---
+++ src/main/java/cn/sucrelt/sucreblog/entity/Blog.java
@@ -0,0 +1,223 @@
+package cn.sucrelt.sucreblog.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * @description: 博客实体类，对应数据库表tb_blog
+ * @author: sucre
+ * @date: 2021/01/11
+ * @time: 16:23
+ */
+
+public class Blog {
+    /**
+     * 博客主键，id
+     */
+    private Long blogId;
+
+    /**
+     * 博客标题
+     */
+    private String blogTitle;
+
+    /**
+     * 博客自定义路径url
+     */
+    private String blogSubUrl;
+
+    /**
+     * 博客封面图
+     */
+    private String blogCoverImage;
+
+    /**
+     * 博客分类id
+     */
+    private Integer blogCategoryId;
+
+    /**
+     * 博客分类名（冗余字段）
+     */
+    private String blogCategoryName;
+
+    /**
+     * 博客标签
+     */
+    private String blogTags;
+
+    /**
+     * 博客状态：0-草稿，1-发布
+     */
+    private Byte blogStatus;
+
+    /**
+     * 博客阅读量
+     */
+    private Long blogViews;
+
+    /**
+     * 是否允许评论 0-允许，1-不允许
+     */
+    private Byte enableComment;
+
+    /**
+     * 是否删除 0-否，1-是
+     */
+    private Byte isDeleted;
+
+    /**
+     * 添加时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 博客内容
+     */
+    private String blogContent;
+
+    public Long getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(Long blogId) {
+        this.blogId = blogId;
+    }
+
+    public String getBlogTitle() {
+        return blogTitle;
+    }
+
+    public void setBlogTitle(String blogTitle) {
+        this.blogTitle = blogTitle == null ? null : blogTitle.trim();
+    }
+
+    public String getBlogSubUrl() {
+        return blogSubUrl;
+    }
+
+    public void setBlogSubUrl(String blogSubUrl) {
+        this.blogSubUrl = blogSubUrl == null ? null : blogSubUrl.trim();
+    }
+
+    public String getBlogCoverImage() {
+        return blogCoverImage;
+    }
+
+    public void setBlogCoverImage(String blogCoverImage) {
+        this.blogCoverImage = blogCoverImage == null ? null : blogCoverImage.trim();
+    }
+
+    public Integer getBlogCategoryId() {
+        return blogCategoryId;
+    }
+
+    public void setBlogCategoryId(Integer blogCategoryId) {
+        this.blogCategoryId = blogCategoryId;
+    }
+
+    public String getBlogCategoryName() {
+        return blogCategoryName;
+    }
+
+    public void setBlogCategoryName(String blogCategoryName) {
+        this.blogCategoryName = blogCategoryName == null ? null : blogCategoryName.trim();
+    }
+
+    public String getBlogTags() {
+        return blogTags;
+    }
+
+    public void setBlogTags(String blogTags) {
+        this.blogTags = blogTags == null ? null : blogTags.trim();
+    }
+
+    public Byte getBlogStatus() {
+        return blogStatus;
+    }
+
+    public void setBlogStatus(Byte blogStatus) {
+        this.blogStatus = blogStatus;
+    }
+
+    public Long getBlogViews() {
+        return blogViews;
+    }
+
+    public void setBlogViews(Long blogViews) {
+        this.blogViews = blogViews;
+    }
+
+    public Byte getEnableComment() {
+        return enableComment;
+    }
+
+    public void setEnableComment(Byte enableComment) {
+        this.enableComment = enableComment;
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getBlogContent() {
+        return blogContent;
+    }
+
+    public void setBlogContent(String blogContent) {
+        this.blogContent = blogContent == null ? null : blogContent.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", blogId=").append(blogId);
+        sb.append(", blogTitle=").append(blogTitle);
+        sb.append(", blogSubUrl=").append(blogSubUrl);
+        sb.append(", blogCoverImage=").append(blogCoverImage);
+        sb.append(", blogCategoryId=").append(blogCategoryId);
+        sb.append(", blogCategoryName=").append(blogCategoryName);
+        sb.append(", blogTags=").append(blogTags);
+        sb.append(", blogStatus=").append(blogStatus);
+        sb.append(", blogViews=").append(blogViews);
+        sb.append(", enableComment=").append(enableComment);
+        sb.append(", isDeleted=").append(isDeleted);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", blogContent=").append(blogContent);
+        sb.append("]");
+        return sb.toString();
+    }
+}
