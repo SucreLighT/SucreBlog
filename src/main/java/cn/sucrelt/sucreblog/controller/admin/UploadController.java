90c86e9958a9395e47293db70e270d23242a8b24
tangtangsama <1462339680@qq.com>
2021-01-19 18:34:00 +0800

    图片上传类

---
+++ src/main/java/cn/sucrelt/sucreblog/controller/admin/UploadController.java
@@ -0,0 +1,63 @@
+package cn.sucrelt.sucreblog.controller.admin;
+
+import cn.sucrelt.sucreblog.config.ConstantsConfig;
+import cn.sucrelt.sucreblog.util.MyBlogUtils;
+import cn.sucrelt.sucreblog.util.Result;
+import cn.sucrelt.sucreblog.util.ResultGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * @description:
+ * @author: sucre
+ * @date: 2021/01/12
+ * @time: 18:31
+ */
+
+@Controller
+@RequestMapping("/admin")
+public class UploadController {
+
+    @PostMapping({"/upload/file"})
+    @ResponseBody
+    public Result upload(HttpServletRequest httpServletRequest, @RequestParam("file") MultipartFile file) throws URISyntaxException {
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //生成文件名称通用方法
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        StringBuilder tempName = new StringBuilder();
+        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+        String newFileName = tempName.toString();
+        File fileDirectory = new File(ConstantsConfig.FILE_UPLOAD_DIC);
+        //创建文件
+        File destFile = new File(ConstantsConfig.FILE_UPLOAD_DIC + newFileName);
+        try {
+            if (!fileDirectory.exists()) {
+                if (!fileDirectory.mkdir()) {
+                    throw new IOException("文件夹创建失败,路径为：" + fileDirectory);
+                }
+            }
+            file.transferTo(destFile);
+            Result resultSuccess = ResultGenerator.generateSuccessResult();
+            resultSuccess.setData(MyBlogUtils.getHost(new URI(httpServletRequest.getRequestURL() + "")) + "/upload/" + newFileName);
+            return resultSuccess;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResultGenerator.generateFailResult("文件上传失败");
+        }
+    }
+}
